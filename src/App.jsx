fa04e63d5dd84b9b21c6cebb4f15aa1a8a13071f
Utkarshalways <utkarshshrma08@gmail.com>
2024-04-14 22:24:10 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ function App() {
  
 
   return (
-    <div className=" h-screen  flex items-center justify-center flex-col bg-slate-900 gap-4">
-      <h1 className=' text-5xl text-white'>Voice Calculator</h1>
+    <div className=" h-screen  flex items-center justify-between flex-col bg-slate-900 ">
+      <h1 className=' text-5xl text-white mt-4'>Sound Solver</h1>
       
       <div className='h-1/2 w-1/3'>
       <VoiceInput />
+      </div>
+
+      <div className='Footer  h-1/4 w-full bg-red-700'>
+      This is the footer of this web page
       </div>
     </div>
   );
